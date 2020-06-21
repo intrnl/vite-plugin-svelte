ae1611e3fb0dde6b2e1e34941f4e8b6121afcfa8
intrnl <intrnl28+git@gmail.com>
2020-06-22 00:18:41 +0700

    rewrite, add hmr support

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -2,34 +2,47 @@ import type { Plugin } from 'vite';
 
 
 interface SveltePreprocessorResult {
-  code: string;
-  map?: object | string;
-  dependencies?: string[];
+  code: string,
+  map?: object | string,
+  dependencies?: string[],
 }
 
 type SveltePreprocessor = (opts: {
-  content: string;
-  attributes: Record<string, string | boolean>;
-  filename?: string;
+  content: string,
+  attributes: Record<string, string | boolean>,
+  filename?: string,
 }) => SveltePreprocessorResult | Promise<SveltePreprocessorResult>;
 
 type SvelteMarkupPreprocessor = (opts: {
-  content: string;
-  filename?: string;
+  content: string,
+  filename?: string,
 }) => SveltePreprocessorResult | Promise<SveltePreprocessorResult>;
 
 interface SveltePreprocessorGroup {
-  markup?: SvelteMarkupPreprocessor;
-  style?: SveltePreprocessor;
-  script?: SveltePreprocessor;
+  markup?: SvelteMarkupPreprocessor,
+  style?: SveltePreprocessor,
+  script?: SveltePreprocessor,
+}
+
+
+interface SvelteHotOptions {
+  noPreserveState?: boolean,
+  noPreserveStateKey?: string,
+  noReload?: boolean,
+  optimistic?: boolean,
+  acceptNamedExports?: boolean,
+  acceptAccessors?: boolean,
+  injectCss?: boolean,
+  cssEjectDelay?: number,
 }
 
 
 interface SveltePluginOptions {
-  dev?: boolean;
-  immutable?: boolean;
-  hydratable?: boolean;
-  preprocess?: SveltePreprocessorGroup | SveltePreprocessorGroup[];
+  dev?: boolean,
+  immutable?: boolean,
+  hydratable?: boolean,
+  preprocess?: SveltePreprocessorGroup | SveltePreprocessorGroup[],
+  hot?: boolean | SvelteHotOptions,
 }
 
 declare function sveltePlugin (options?: SveltePluginOptions): Plugin;
